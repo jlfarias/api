1a084945a838f3ac28403574e9071b1e66cfcfe8
jlfarias <jlfarias2009@gmail.com>
2021-09-25 17:54:56 -0300

    Add files via upload

--- models/materia.js
+++ models/materia.js
@@ -1,11 +1,92 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const materia = sequelize.define('materia', {
-    nombre: DataTypes.STRING,
-    id_carrera: DataTypes.INTEGER
-  }, {});
-  materia.associate = function(models) {
-    // associations can be defined here
-  };
-  return materia;
+var express = require("express");
+var router = express.Router();
+var models = require("../models");
+
+// GET
+router.get("/", (req, res) => {
+  console.log("Esto es un mensaje para ver en consola");
+  models.materia
+    .findAll({
+      attributes: ["id", "nombre", "id_carrera"]
+    })
+    .then(materia => res.send(materia))
+    .catch(() => res.sendStatus(500));
+});
+
+
+// POST
+router.post("/", (req, res) => {
+  models.materia
+    .create({ nombre: req.body.nombre, id_carrera: req.body.id_carrera})
+    .then(materia => res.status(201).send({ id: materia.id }))
+    .catch(error => {
+      if (error == "SequelizeUniqueConstraintError: Validation error") {
+        res.status(400).send('Bad request: existe otra materia con el mismo nombre')
+      }
+      else {
+        console.log(`Error al intentar insertar en la base de datos: ${error}`)
+        res.sendStatus(500)
+      }
+    });
+});
+
+// ENCONTRAR MATERIA
+const findMateria = (id, { onSuccess, onNotFound, onError }) => {
+  models.materia
+    .findOne({
+      attributes: ["id", "nombre", "id_carrera"],
+      where: { id }
+    })
+    .then(materia => (materia ? onSuccess(materia) : onNotFound()))
+    .catch(() => onError());
 };
+
+
+// GET POR ID
+router.get("/:id", (req, res) => {
+  findMateria(req.params.id, {
+    onSuccess: materia => res.send(materia),
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
+});
+
+
+// ACTUALIZAR MATERIA 
+router.put("/:id", (req, res) => {
+  const onSuccess = materia =>
+    materia
+      .update({ nombre: req.body.nombre }, { fields: ["nombre"] })
+      .then(() => res.sendStatus(200))
+      .catch(error => {
+        if (error == "SequelizeUniqueConstraintError: Validation error") {
+          res.status(400).send('Bad request: existe otra materia con el mismo nombre')
+        }
+        else {
+          console.log(`Error al intentar actualizar la base de datos: ${error}`)
+          res.sendStatus(500)
+        }
+      });
+    findMateria(req.params.id, {
+    onSuccess,
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
+});
+
+
+// BORRAR MATERIA
+router.delete("/:id", (req, res) => {
+  const onSuccess = materia =>
+    materia
+      .destroy()
+      .then(() => res.sendStatus(200))
+      .catch(() => res.sendStatus(500));
+  findMateria(req.params.id, {
+    onSuccess,
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
+});
+
+module.exports = router;
