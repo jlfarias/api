e4b2c2137dcfbf50bdff7a8aa80f758fbfbe9e64
jlfarias <jlfarias2009@gmail.com>
2021-10-24 14:57:58 -0300

    corregido profesor

--- models/profesor.js
+++ models/profesor.js
@@ -7,14 +7,14 @@ module.exports = (sequelize, DataTypes) => {
   profesor.associate = function(models) {
     // associations can be defined here
       	//asociacion a carrera (pertenece a:)
-      /*
-        profesor.belongsTo(models.profesor// modelo al que pertenece
+      /**/
+        profesor.belongsTo(models.materia// modelo al que pertenece
           ,{
             as : 'Materia-Relacionada',  // nombre de mi relacion
             foreignKey: 'id_materia'     // campo con el que voy a igualar
           })
           /////////////////////
-      */
+      
   };
   return profesor;
 };
